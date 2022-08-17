7ca1a3633ecef441023068031dcab4255a45286a
Daniel Suen <danielsuen200791357@gmail.com>
2022-08-17 22:27:13 +0000

    installed prettier as dec dependecy and ran it

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $("#tweet-text").on("input", function () {
     let inputLength = $(this).val().length;
-    const letterCounter = $("#tweet-text").siblings("div").children(".counter")
+    const letterCounter = $("#tweet-text").siblings("div").children(".counter");
     letterCounter.val((i, val) => {
       let maximumCount = 140;
-      return val = maximumCount - inputLength;
-      });
+      return (val = maximumCount - inputLength);
+    });
     if (letterCounter.val() <= 0) {
       letterCounter.addClass("negative");
     }
-  })
+  });
 });
