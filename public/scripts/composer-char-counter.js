552525638a265543e7d6391914be849f3feaf164
Daniel Suen <danielsuen200791357@gmail.com>
2022-08-17 23:50:40 +0000

    added comments to CSS

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-  $("#tweet-text").on("input", function () {
+  $("#tweet-text").on("input", function () { 
     let inputLength = $(this).val().length;
-    const letterCounter = $("#tweet-text").siblings("div").children(".counter");
+    const letterCounter = $("#tweet-text").siblings("div").children(".counter"); //corresponds to output element in index html
     letterCounter.val((i, val) => {
       let maximumCount = 140;
       return (val = maximumCount - inputLength);
     });
     if (letterCounter.val() <= 0) {
-      letterCounter.addClass("negative");
+      letterCounter.addClass("negative");//changes color of counter when value is negative
     }
   });
 });
--- public/scripts/tweetbody.js
+++ public/scripts/tweetbody.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
   let heart = $(".fa-heart");
-  heart.mouseenter(function () {
+  heart.mouseenter(function () {//changes icon on mouseover
     $(this).addClass("fa-heart-pulse");
     $(this).removeClass("fa-heart");
   });
-  heart.mouseleave(function () {
+  heart.mouseleave(function () {//changes icon back on mouseleave
     $(this).addClass("fa-heart");
     $(this).removeClass("fa-heart-pulse");
   });
